7d4652a96df4d6f193503f5c1f1dce88fea6f8d7
Chelsea <chelseacmichaels@gmail.com>
2013-08-04 20:05:18 -0500

    updating files

---
+++ application.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.button').mouseenter(function() {
+        $(this).css("background-color","#a53f33");
+    });
+    $('.button').mouseleave(function(){
+        $(this).css("background-color","#dd5544");
+    
+    });
+});
